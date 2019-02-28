c8a781e4fb1cfbf9b8502ffcb7eba40483f63eb6
Ben Spector <ben@operationspark.org>
2019-02-28 21:08:18 +0000

    removed 6 functions from function-guru and moved 4 of them into an extra credit folder

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -13,40 +13,6 @@ describe("Function Guru", function () {
 
     // const functions = require(path);
 
-    describe('objectValues()', function() {
-      it('Should take an object and return its values in an array', function() {
-        var objectOne = {a: "one", b: "two", ponies: "crayons", dingle: "dangle"};
-        var objectTwo = {c: "three", d: "four", crayons: "ponies", dangle: "dingle"};
-        assert.deepEqual(objectValues(objectOne), ["one","two","crayons","dangle"]);
-        assert.deepEqual(objectValues(objectTwo), ["three","four","ponies","dingle"]);
-      });
-    });
-
-    describe('keysToString()', function() {
-      it('Should take an object and return all its keys in a string each separated with a space', function() {
-        var objectOne = {a: "one", b: "two", ponies: "crayons", dingle: "dangle"};
-        var objectTwo = {c: "three", d: "four", crayons: "ponies", dangle: "dingle"};
-        assert.equal(keysToString(objectOne), "a b ponies dingle");
-        assert.equal(keysToString(objectTwo), "c d crayons dangle");
-      });
-    });
-
-    describe('valuesToString()', function() {
-      it('Should take an object and return all its string values in a string each separated with a space', function() {
-        var objectOne = {a: "one", b: "two", ponies: "crayons", something: {}, dingle: "dangle"};
-        var objectTwo = {c: "three", boolean: false, d: "four", crayons: "ponies", dangle: "dingle"};
-        assert.equal(valuesToString(objectOne), "one two crayons dangle");
-        assert.equal(valuesToString(objectTwo), "three four ponies dingle");
-      });
-    });
-
-    describe('arrayOrObject()', function() {
-      it('Should take one argument and return \'array\' if its an array and \'object\' if it is an object', function() {
-        assert.equal(arrayOrObject({a:"one"}), "object");
-        assert.equal(arrayOrObject([1,2,{}]), "array");
-      });
-    });
-
     describe('capitalizeWord()', function() {
       it('Should take a string of one word, and return the word with its first letter capitalized', function() {
         assert.equal( capitalizeWord("greg"), "Greg");
@@ -76,14 +42,6 @@ describe("Function Guru", function () {
       });
     });
 
-    describe('maybeNoises()', function() {
-      it('Should take an object, if this object has a noises array return them as a string separated by a space, if there are no noises return \'there are no noises\'', function() {
-        assert.equal( maybeNoises({noises:["bark", "woof", "squeak","growl"]}), "bark woof squeak growl");
-        assert.equal( maybeNoises({noises: []}), "there are no noises");
-        assert.equal( maybeNoises({}), "there are no noises");
-      });
-    });
-
     describe('hasWord()', function() {
       it('Should take a string of words and a word and return true if <word> is in <string of words>, otherwise return false.', function() {
         var data = "This is a super awesome string of words";
@@ -105,20 +63,6 @@ describe("Function Guru", function () {
         assert.equal( isFriend("jimmy",{friends:["bobby", "ralf"]}), false);
         assert.equal( isFriend("ralf",{friends:["bobby", "ralf"]}), true);
         assert.equal( isFriend("chuck",{}), false);
-      });
-    });
-
-    describe('nonFriends()', function() {
-      it('Should take a name and a list of people, and return a list of all the names that <name> is not friends with', function() {
-        var data = [
-          {name: "Jimmy", friends:["Sara", "Liza"]},
-          {name: "Bob", friends:[]},
-          {name: "Liza", friends: ["Jimmy"]},
-          {name: "Sara", friends: ["Jimmy"]}
-        ];
-        assert.deepEqual( nonFriends("Jimmy", data), ["Bob"]);
-        assert.deepEqual( nonFriends("Bob", data), ["Jimmy", "Liza", "Sara"]);
-        assert.deepEqual( nonFriends("Sara", data), ["Bob","Liza"]);
       });
     });
 
